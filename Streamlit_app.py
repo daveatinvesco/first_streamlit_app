b20ee694e9205a66bd18770f1adb19a3b265fa03
daveatinvesco <136648189+daveatinvesco@users.noreply.github.com>
2023-06-15 14:12:59 -0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My parents new healthy diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-boiled Free-range egg')
+streamlit.header('Breakfast Favorites)
+streamlit.text('🫐 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥬 Kale, Spinach & Rocket Smoothie')
+streamlit.text('Avocado 🥑 toast')
